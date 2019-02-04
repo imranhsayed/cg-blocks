78e4c9f7f9692d1fc8f8db8b09e94f130e6a5ae3
Imran Sayed <imranhsayed@gmail.com>
2019-02-04 23:40:10 +0530

    Check php version and include class file
    Check PHP version. If the PHP version below than 5.3, throw an error and include class-register-blocks.php.

--- cg-blocks.php
+++ cg-blocks.php
@@ -18,3 +18,36 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'CB_PLUGIN_URL', plugins_url( 'cg-blocks' ) );
 define( 'CB_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+
+if ( version_compare( phpversion(), '5.3', '>=' ) ) {
+	require_once 'inc/class-register-blocks.php';
+	/**
+	 * Registers Custom Card block.
+	 */
+	function cf_register_blocks() {
+		new \cg_blocks\Register_Blocks();
+	}
+	add_action( 'plugins_loaded', 'cf_register_blocks' );
+} else {
+	if ( defined( 'WP_CLI' ) ) {
+		WP_CLI::warning( _cg_gutenberg_blocks_php_version_text() );
+	} else {
+		add_action( 'admin_notices', '_cg_gutenberg_blocks_php_version_error' );
+	}
+}
+
+/**
+ * Admin notice for incompatible versions of PHP.
+ */
+function _cg_gutenberg_blocks_php_version_error() {
+	printf( '<div class="error"><p>%s</p></div>', esc_html( _cg_gutenberg_blocks_php_version_text() ) );
+}
+
+/**
+ * String describing the minimum PHP version.
+ *
+ * @return string
+ */
+function _cg_gutenberg_blocks_php_version_text() {
+	return esc_html__( 'Cb Gutenberg Blocks plugin error: Your version of PHP is too old to run this plugin. You must be running PHP 5.3 or higher.', 'cg-blocks' );
+}
