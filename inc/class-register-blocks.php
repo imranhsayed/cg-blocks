fffcc8b57c20aed07cbd4f9f90a9a028c6161387
Imran Sayed <imranhsayed@gmail.com>
2019-02-04 23:46:14 +0530

    Add languages directory and call load_plugin_textdomain() to load all translations

--- inc/class-register-blocks.php
+++ inc/class-register-blocks.php
@@ -23,5 +23,14 @@ class Register_Blocks {
 			// Gutenberg is not active.
 			return;
 		}
+		add_action( 'init', array( $this, 'load_textdomain' ) );
 	}
+
+	/**
+	 * Load all translations for our plugin from the MO file.
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'cg-blocks', false, CB_PLUGIN_DIR_PATH . 'languages' );
+	}
+
 }
