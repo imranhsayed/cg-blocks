2efb03b6c65da46df4c8fb85450c22799e1a8475
Imran Sayed <imranhsayed@gmail.com>
2019-02-04 23:38:28 +0530

    Create inc directory and create class file class-register-blocks.php

---
+++ inc/class-register-blocks.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class Register_Blocks
+ *
+ * @package Gutenberg Contact Card Block
+ */
+
+namespace cg_blocks;
+
+/**
+ * Class Register_Blocks
+ *
+ * @package Gutenberg Contact Card Block
+ */
+class Register_Blocks {
+
+}
