72b87a74c16d06d97d5cd0bd7f629256800f095b
Imran Sayed <imranhsayed@gmail.com>
2019-02-04 23:31:22 +0530

    Add the main plugin file cg-blocks.php and add file header

---
+++ cg-blocks.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Gutenberg Contact Card Block
+ * Plugin URI: https://imransayed.com
+ * Description: This plugin creates a custom gutenberg contact card block
+ * Version: 1.0.0
+ * Author: Imran Sayed
+ * Author URI: https://profiles.wordpress.org/gsayed786
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: cg-blocks
+ * Domain Path: /languages
+ *
+ * @package Gutenberg Contact Card Block
+ */
+
+defined( 'ABSPATH' ) || exit;
